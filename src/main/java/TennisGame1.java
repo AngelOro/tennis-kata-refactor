95b1ab7493ac0492c7f813644c198f14ffdbe014
AngeOro <angelica-om@outlook.com>
2021-03-28 20:00:23 -0500

    Se agregan comentarios a TennisGame1

--- src/main/java/TennisGame1.java
+++ src/main/java/TennisGame1.java
@@ -2,24 +2,36 @@ public class TennisGame1 implements TennisGame {
     
     private int player1Score = 0;
     private int player2Score = 0;
+    private String player1Name;
+    private String player2Name;
     private final String[] scores = {"Love","Fifteen","Thirty","Forty"};
 
     public TennisGame1(String player1Name, String player2Name) {
+        this.player1Name = player1Name;
+        this.player2Name = player2Name;
     }
 
+    /*
+    * Sumatoria de puntos a cada jugador
+    * player1Score -> Puntaje jugador 1
+    * player2Score -> Puntaje jugador 2
+    * */
     public void wonPoint(String playerName) {
-        if (playerName.equals("player1"))
-            player1Score += 1;
+        if (playerName == player1Name)
+            player1Score ++;
         else
-            player2Score += 1;
+            player2Score ++;
     }
 
+    /*
+    * Obtiene resultado del juego
+    * */
     public String getScore() {
         String score;
-        if (player1Score == player2Score) {
-            score = tieGame(player1Score);
+        if (isTie()) {
+            score = tieGame();
         }
-        else if (player1Score >= 4 || player2Score >= 4) {
+        else if (isEndGame()) {
             score = gameAdvantage();
         }
         else {
@@ -28,11 +40,20 @@ public class TennisGame1 implements TennisGame {
         return score;
     }
 
+    /*
+    * Verifica si está en empate
+    * */
+    public boolean isTie(){
+        return player1Score == player2Score;
+    }
 
-    public String tieGame (int gameScore){
+    /*
+    * Devuelve el mensaje de empate de acuerdo al array scores[]
+    * */
+    public String tieGame (){
         String score;
-        if(gameScore <= 2){
-            score = scores[gameScore] + "-All";
+        if(player1Score <= 2){
+            score = scores[player1Score] + "-All";
         }
         else {
             score = "Deuce";
@@ -40,10 +61,23 @@ public class TennisGame1 implements TennisGame {
         return score;
     }
 
+    /*
+    * Verifica si el juego terminó
+    * */
+    public boolean isEndGame(){
+        return player1Score >= 4 || player2Score >= 4;
+    }
+
+    /*
+    * Se obtiene la diferencia de puntos entre los jugadores
+    * */
     public int differenceResult(){
         return player1Score - player2Score;
     }
 
+    /*
+    * Si el juego termina con ventaja de 1 punto devuelve mensaje de ganador
+    * */
     public String gameAdvantage(){
         String score;
         if (differenceResult()==1){
@@ -57,6 +91,10 @@ public class TennisGame1 implements TennisGame {
         }
         return score;
     }
+
+    /*
+    * Si el juego termina con ventaja de 2 o más puntos devuelve mensaje de ganador
+    * */
     public String winner(){
         String score;
         if (differenceResult()>=2) {
@@ -68,8 +106,10 @@ public class TennisGame1 implements TennisGame {
         return score;
     }
 
+    /*
+    * Devuelve el puntaje actual del juego
+    * */
     public String currentScore(){
         return scores[player1Score] + "-" +scores[player2Score];
     }
-
 }
