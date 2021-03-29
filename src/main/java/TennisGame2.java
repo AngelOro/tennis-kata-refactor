ee498df5b01f5736ea9d9217690fb7739c274a2c
AngeOro <angelica-om@outlook.com>
2021-03-28 19:59:19 -0500

    Refactorización a TennisGame2

--- src/main/java/TennisGame2.java
+++ src/main/java/TennisGame2.java
@@ -1,135 +1,122 @@
 
 public class TennisGame2 implements TennisGame
 {
-    public int P1point = 0;
-    public int P2point = 0;
-    
-    public String P1res = "";
-    public String P2res = "";
-    private String player1Name;
-    private String player2Name;
+    private int player1Score;
+    private int player2Score;
+
+    private final String player1Name;
+    private final String player2Name;
+    private final String[] scores = {"Love","Fifteen","Thirty","Forty"};
 
     public TennisGame2(String player1Name, String player2Name) {
         this.player1Name = player1Name;
         this.player2Name = player2Name;
     }
 
+    /*
+     * Sumatoria de puntos a cada jugador
+     * player1Score -> Puntaje jugador 1
+     * player2Score -> Puntaje jugador 2
+     * */
+    public void wonPoint(String player) {
+        if (player.equals(player1Name)){
+            player1Score++;
+        } else {
+            player2Score++;
+        }
+    }
+
+    /*
+     * Obtiene resultado del juego
+     * */
     public String getScore(){
-        String score = "";
-        if (P1point == P2point && P1point < 4)
+        String messageScore = "";
+        if (isTie()) messageScore = gameTie();
+        
+        if(!isTie() && isCurrentGame()) messageScore = currentScore();
+
+        if(!isTie() && isEndGame()) messageScore = gameAdvantage();
+
+        return messageScore;
+    }
+
+    /*
+     * Verifica si está en empate
+     * */
+    public boolean isTie(){
+        return player1Score == player2Score;
+    }
+
+    /*
+     * Devuelve el mensaje de empate de acuerdo al array scores[]
+     * */
+    public String gameTie(){
+        String score;
+        if (player1Score < 3)
         {
-            if (P1point==0)
-                score = "Love";
-            if (P1point==1)
-                score = "Fifteen";
-            if (P1point==2)
-                score = "Thirty";
-            score += "-All";
-        }
-        if (P1point==P2point && P1point>=3)
+            score = scores[player1Score] + "-All";
+        }else {
             score = "Deuce";
-        
-        if (P1point > 0 && P2point==0)
-        {
-            if (P1point==1)
-                P1res = "Fifteen";
-            if (P1point==2)
-                P1res = "Thirty";
-            if (P1point==3)
-                P1res = "Forty";
-            
-            P2res = "Love";
-            score = P1res + "-" + P2res;
-        }
-        if (P2point > 0 && P1point==0)
-        {
-            if (P2point==1)
-                P2res = "Fifteen";
-            if (P2point==2)
-                P2res = "Thirty";
-            if (P2point==3)
-                P2res = "Forty";
-            
-            P1res = "Love";
-            score = P1res + "-" + P2res;
-        }
-        
-        if (P1point>P2point && P1point < 4)
-        {
-            if (P1point==2)
-                P1res="Thirty";
-            if (P1point==3)
-                P1res="Forty";
-            if (P2point==1)
-                P2res="Fifteen";
-            if (P2point==2)
-                P2res="Thirty";
-            score = P1res + "-" + P2res;
-        }
-        if (P2point>P1point && P2point < 4)
-        {
-            if (P2point==2)
-                P2res="Thirty";
-            if (P2point==3)
-                P2res="Forty";
-            if (P1point==1)
-                P1res="Fifteen";
-            if (P1point==2)
-                P1res="Thirty";
-            score = P1res + "-" + P2res;
-        }
-        
-        if (P1point > P2point && P2point >= 3)
-        {
-            score = "Advantage player1";
-        }
-        
-        if (P2point > P1point && P1point >= 3)
-        {
-            score = "Advantage player2";
-        }
-        
-        if (P1point>=4 && P2point>=0 && (P1point-P2point)>=2)
-        {
-            score = "Win for player1";
-        }
-        if (P2point>=4 && P1point>=0 && (P2point-P1point)>=2)
-        {
-            score = "Win for player2";
         }
         return score;
     }
-    
-    public void SetP1Score(int number){
-        
-        for (int i = 0; i < number; i++)
-        {
-            P1Score();
-        }
-            
-    }
-    
-    public void SetP2Score(int number){
-        
-        for (int i = 0; i < number; i++)
-        {
-            P2Score();
-        }
-            
-    }
-    
-    public void P1Score(){
-        P1point++;
-    }
-    
-    public void P2Score(){
-        P2point++;
+
+    /*
+    * Verifica si aún se está jugando
+    * */
+    public boolean isCurrentGame(){
+       return player1Score < 4 && player2Score < 4;
     }
 
-    public void wonPoint(String player) {
-        if (player == "player1")
-            P1Score();
-        else
-            P2Score();
+    /*
+     * Devuelve el puntaje actual del juego
+     * */
+    public String currentScore(){
+        return scores[player1Score] + "-" + scores[player2Score];
     }
+
+    /*
+     * Se obtiene la diferencia de puntos entre los jugadores
+     * */
+    public int differenceResult(){
+        return player1Score - player2Score;
+    }
+
+    /*
+     * Se obtiene la diferencia de puntos entre los jugadores
+     * */
+    public boolean isEndGame(){
+        return player1Score >= 4 || player2Score >= 4;
+    }
+
+    /*
+     * Si el juego termina con ventaja de 1 punto devuelve mensaje de ganador
+     * */
+    public String gameAdvantage(){
+        String score;
+        if (differenceResult() == 1){
+            score ="Advantage player1";
+        }
+        else if (differenceResult() == -1) {
+            score ="Advantage player2";
+        }else{
+            score = winner();
+        }
+        return score;
+    }
+
+    /*
+     * Si el juego termina con ventaja de 2 o más puntos devuelve mensaje de ganador
+     * */
+    public String winner(){
+        String score;
+        if (differenceResult()>=2) {
+            score = "Win for player1";
+        }
+        else {
+            score ="Win for player2";
+        }
+        return score;
+    }
+
 }
